d03afd3c5a08660f3aba0d304cf0b3f661f5c9f0
YokotaYoshi <ymdtr5274@gmail.com>
2025-01-09 16:24:41 +0900

    w

--- WebMakingPractice/JS_Data-2/Practice/chapter3-colorpicker/js/script.js
+++ WebMakingPractice/JS_Data-2/Practice/chapter3-colorpicker/js/script.js
@@ -1,7 +1,15 @@
 console.log(document.querySelector("#colorPicker").value);
-document.querySelector("#colorText").textContent = document.querySelector("#colorPicker").value;
+/*document.querySelector("#colorText").textContent = "カラーコード："+document.querySelector("#colorPicker").value;*/
+/*document.querySelector("#colorText").textContent=`カラーコード：${document.querySelector("#colorPicker").value}`;*/
+
+const name="Mana";
+const text=document.querySelector("#colorText");
+const color=document.querySelector("#colorPicker");
+console.log(name);
+
+text.textContent=`カラーコード：${color.value}`;
+
 //1行コメントアウト
 /*
 複数行コメントアウト
 */
-
